f7b41740ee919ac15eb3e1fd1796280cb0a54b41
João Rocha <71012028+jpa-rocha@users.noreply.github.com>
2023-03-05 16:45:48 +0100

    Update astro23.py

--- astro23.py
+++ astro23.py
@@ -18,6 +18,14 @@ blu = (135, 206, 250)
 sch = (0, 0, 0)
 wei = (255, 255, 255)
 
+# Set up the colour sensor
+sense.color.gain = 60 # Set the sensitivity of the sensor
+sense.color.integration_cycles = 64 # The interval at which the reading will be taken
+
+# Add colour variables and image
+col = sense.color.color
+# Display the image
+
 wahl = []
 
 
@@ -142,14 +150,182 @@ wahl.append([
     blu, sch, sch, sch, sch, sch, blu, sch,
     rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
 ])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, sch, sch, blu, blu, blu, blu,
+    blu, sch, sch, wei, sch, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, sch, sch, blu, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, blu, blu, blu, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, blu, blu, blu, blu, blu, sch,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, sch, blu,
+    blu, blu, blu, blu, blu, blu, sch, sch,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, sch, blu, blu,
+    pur, blu, blu, blu, blu, sch, sch, blu,
+    blu, blu, blu, blu, sch, sch, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, sch, blu, blu,
+    rbl, pur, blu, sch, sch, blu, blu, blu,
+    pur, blu, blu, blu, sch, blu, blu, blu,
+    blu, blu, blu, blu, sch, sch, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, sch, blu, blu,
+    pur, blu, blu, sch, sch, blu, blu, blu,
+    blu, blu, blu, blu, sch, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, sch, blu, blu, blu,
+    blu, blu, blu, sch, sch, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, blu, blu, sch, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, blu, blu, blu, blu, blu,
+    rbl, pur, blu, blu, blu, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, blu, blu, blu, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, rbl, pur, blu, blu, blu, blu,
+    hgr, rbl, pur, col, col, blu, blu, blu,
+    rbl, pur, blu, col, col, blu, blu, blu,
+    pur, blu, blu, blu, blu, blu, blu, blu,
+    blu, blu, blu, blu, blu, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, gel, hgr, rbl, pur, blu, blu, blu,
+    gel, hgr, col, col, col, col, blu, blu,
+    hgr, rbl, col, sch, sch, col, blu, blu,
+    rbl, pur, col, sch, sch, col, blu, blu,
+    pur, blu, col, col, col, col, blu, blu,
+    blu, blu, blu, blu, blu, blu, blu, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    rot, org, gel, hgr, rbl, pur, blu, blu,
+    org, col, col, col, col, col, col, blu,
+    gel, col, sch, sch, sch, sch, col, blu,
+    hgr, col, sch, sch, sch, sch, col, blu,
+    rbl, col, sch, sch, sch, sch, col, blu,
+    pur, col, sch, sch, sch, sch, col, blu,
+    blu, col, col, col, col, col, col, blu,
+    rbl, rbl, rbl, rbl, rbl, rbl, rbl, rbl
+])
+
+wahl.append([
+    col, col, col, col, col, col, col, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, sch, sch, sch, sch, sch, sch, col,
+    col, col, col, col, col, col, col, col
+])
+
+wahl.append([
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch,
+    sch, sch, sch, sch, sch, sch, sch, sch
+])
 for i in range(0, len(wahl)):
     sense.set_pixels(wahl[i])
-    sleep(1)
-
-# Set up the colour sensor
-#sense.color.gain = 60 # Set the sensitivity of the sensor
-#sense.color.integration_cycles = 64 # The interval at which the reading will be taken
-
-# Add colour variables and image
-
-# Display the image
+    sleep(0.001)
